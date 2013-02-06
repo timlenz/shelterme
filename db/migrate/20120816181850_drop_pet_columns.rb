5b66436381097293c63ff0cd03f37c95922ddc24
Tim Lenz <tim@scimantics.com>
2013-02-05 22:13:44 -0500

    migration fix

--- db/migrate/20120816181850_drop_pet_columns.rb
+++ db/migrate/20120816181850_drop_pet_columns.rb
@@ -2,21 +2,13 @@ class DropPetColumns < ActiveRecord::Migration
   def up
     remove_index "pets", :column => "mix_id"
     remove_index "pets", :column => "age_id"
-    remove_index "pets", :column => "coat_id"
-    remove_index "pets", :column => "pet_persona_id"
     remove_column :pets, :mix_id
     remove_column :pets, :age_id
-    remove_column :pets, :coat_id
-    remove_column :pets, :pet_persona_id
   end
 
   def down
-    add_column :pets, :pet_persona_id, :integer
-    add_column :pets, :coat_id, :integer
     add_column :pets, :age_id, :integer
     add_column :pets, :mix_id, :integer
-    add_index :pets, :pet_persona_id
-    add_index :pets, :coat_id
     add_index :pets, :age_id
     add_index :pets, :mix_id
   end
